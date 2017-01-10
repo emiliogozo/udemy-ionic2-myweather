99d51ceb8a8653a692b5e87a5735b377051e8c43
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-01-10 14:27:14 +0800

    feat: Set default location on settings

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+import { WeatherPage } from '../weather/weather';
+import { WeatherService } from '../../app/services/weather.service';
+
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -7,9 +10,45 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'settings.html'
 })
 export class SettingsPage {
+  results: any;
+  searchStr: String;
+  defaultLocation: any;
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    private weatherService: WeatherService) {
 
+  }
+
+  ngOnInit() {
+    this.getDefaultLocation();
+  }
+
+  getDefaultLocation() {
+    if (localStorage.getItem('location') !== undefined) {
+      this.defaultLocation = JSON.parse(localStorage.getItem('location')).name;
+    } else {
+      this.defaultLocation = '10001.11.99999';
+    }
+  }
+
+  setDefaultLocation(location) {
+    this.results = [];
+
+    localStorage.setItem('location', JSON.stringify(location));
+    this.searchStr = location.name;
+    this.getDefaultLocation();
+  }
+
+  getQuery() {
+    this.weatherService.searchCities(this.searchStr)
+      .subscribe(res => {
+        this.results = res.RESULTS;
+      });
+  }
+
+  saveChanges() {
+    this.navCtrl.push(WeatherPage);
   }
 
 }
